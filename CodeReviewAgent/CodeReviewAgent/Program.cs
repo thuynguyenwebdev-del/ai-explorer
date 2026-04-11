cb48f06d3302bba039906cdc91a9b8317d3e766a
Thuy Nguyen <nthuy.wd@gmail.com>
2026-04-11 21:12:50 +0700

    Add CodeReviewAgent project - Create a ChatClient instance to interact with the GPT model - Log the response - Utilize DefaultAzureCredential to authenticate and authorize to the model

---
+++ CodeReviewAgent/CodeReviewAgent/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Extensions.AI;
+
+DefaultAzureCredentialOptions options = new DefaultAzureCredentialOptions
+{
+    TenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID") ?? "default",
+};
+
+IChatClient client = new AzureOpenAIClient(new Uri(
+    Environment.GetEnvironmentVariable("OPENAI_ENDPOINT") ?? "default"),
+    new DefaultAzureCredential(options))
+    //new System.ClientModel.ApiKeyCredential(
+    //    Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? "default"))
+    .GetChatClient("gpt-4o-mini")
+    .AsIChatClient();
+
+var response = await client.GetResponseAsync("Where is the biggest beaver in the world?");
+
+Console.WriteLine(response);
